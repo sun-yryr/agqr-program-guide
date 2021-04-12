c302b9c530ef94e9376e3133baf75eba6b690a15
sun-yryr <taittide@gmail.com>
2021-04-12 23:59:50 +0900

    :sparkles: feat: vapor

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "agqr-program-guide",
+    platforms: [
+       .macOS(.v10_15)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        // HTML Parser
+        .package(url: "https://github.com/tid-kijyun/Kanna.git", .upToNextMajor(from: "5.2.4"))
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Kanna", package: "Kanna")
+            ],
+            swiftSettings: [
+                // Enable better optimizations when building in Release configuration. Despite the use of
+                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
+                // builds. See <https://github.com/swift-server/guides#building-for-produswiftction> for details.
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ]
+        ),
+        .target(name: "Run", dependencies: [.target(name: "App")]),
+        .testTarget(name: "AppTests", dependencies: [
+            .target(name: "App"),
+            .product(name: "XCTVapor", package: "vapor"),
+        ])
+    ]
+)
---
+++ tools/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "Tools",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-format", .branch("swift-5.3-branch")),
+        .package(url: "https://github.com/realm/SwiftLint.git", .upToNextMajor(from: "0.43.1"))
+    ]
+)
