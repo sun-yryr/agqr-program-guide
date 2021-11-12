2c4a99e1c1f96f989295dcc81e30b557560bf204
sun-yryr <taittide@gmail.com>
2021-11-12 14:47:10 +0900

    :bug: fix: assume-roleの設定が変わった

--- terraform/module/github_actions_iam_role/main.tf
+++ terraform/module/github_actions_iam_role/main.tf
@@ -13,17 +13,17 @@ data "aws_iam_policy_document" "this_assume" {
       type = "Federated"
       identifiers = [ aws_iam_openid_connect_provider.github_oidc_provider.arn ]
     }
-    # condition {
-    #   test = "StringLike"
-    #   variable = "vstoken.actions.githubusercontent.com:sub"
-    #   values = [ "repo:${var.github_owner_name}/${var.github_repository_name}" ]
-    # }
+    condition {
+      test = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [ "repo:${var.github_owner_name}/${var.github_repository_name}:*" ]
+    }
   }
 }
 
 resource "aws_iam_openid_connect_provider" "github_oidc_provider" {
   url = "https://token.actions.githubusercontent.com"
-  client_id_list = [ "sigstore" ]
+  client_id_list = [ "https://github.com/${var.github_owner_name}" ]
   thumbprint_list = [ "a031c46782e6e6c662c2c87c76da9aa62ccabd8e" ]
 }
 
