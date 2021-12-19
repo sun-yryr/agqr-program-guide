1de7f793e70eddbbae6664c0b7b95f491f12efa5
sun-yryr <taittide@gmail.com>
2021-12-19 19:36:08 +0900

    :package: update: swift5.5

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "agqr-program-guide",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v12)
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
