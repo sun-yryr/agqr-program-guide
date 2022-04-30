a2d0862ce07767fc4386a952ce88cae12168bc51
sun-yryr <taittide@gmail.com>
2022-04-30 11:33:41 +0900

    feat: テスト用のインフラ作成

---
+++ docker/mysql/01_create_databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS `agqr_program_guide`;
+GRANT ALL ON `agqr_program_guide`.* TO 'vapor_username'@'%';
+CREATE DATABASE IF NOT EXISTS `test_agqr_program_guide`;
+GRANT ALL ON `test_agqr_program_guide`.* TO 'vapor_username'@'%';
